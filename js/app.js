b6eeee492a9d9489487f7880ce8c0aae7f3b8549
Fiona-T <fitracey@gmail.com>
2021-09-08 15:47:07 +0000

    add example code to app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 console.log('Hello World');
+
+// below were done in the browser to show getting elements etc. from the DOM and manipulating them
+let li = document.createElement('li');
+li.classList.add('last');
+li.style.backgroundColor = 'pink';
+let a = document.createElement('a');
+a.textContent = "Four";
+a.style.color = 'white';
+li.appendChild(a);
+let uls = document.getElementsByTagName('ul');
+let ul = uls[0];
+ul.appendChild(li);
+let ulFirst = uls[0];
+let allLi = ulFirst.getElementsByTagName('li');
+let firstLi = allLi[0];
+let lastLi = allLi[3];
+firstLi.classList.remove('active');
+ulFirst.insertBefore(lastLi, firstLi);
+let listItems = document.getElementsByTagName('li');
+for (let i = 0; i < listItems.length; i++) {
+    listItems[i].style.backgroundColor = 'pink'
+}
